81c2149db49401f63c2741abcc67fe11121e59eb
seth <117851610+Sethispr@users.noreply.github.com>
2026-03-08 16:09:50 -0700

    chore: update time.go to mark removal
    https://golang.bg/src/runtime/time_nofake.go#:~:text=15%20var%20faketime%20int64%2016,for%20a%20fast%20monotonic%20time.

--- time.go
+++ time.go
@@ -7,6 +7,9 @@ package protego
 //nolint:revive // Required for go:linkname.
 import _ "unsafe"
 
+// we will mark this to be removed soon as its not beneficial.
+// it is documented more clearly here 
+// https://golang.bg/src/runtime/time_nofake.go#:~:text=15%20var%20faketime%20int64%2016,for%20a%20fast%20monotonic%20time. 
 // nowNano returns monotonic nanoseconds since program start.
 //
 //go:linkname nowNano runtime.nanotime
