d99b10b910e2c2bd48adb403db6b87a7fe7e3972
seth <117851610+Sethispr@users.noreply.github.com>
2026-03-09 19:40:52 -0700

    feat: update time.go and remove go:linkname

--- time.go
+++ time.go
@@ -1,16 +1,11 @@
-//go:build gc
-
 package protego
 
-// Required import for go:linkname to access runtime.nanotime.
-// This allows us to use runtime.nanotime for monotonic time.
-//nolint:revive // Required for go:linkname.
-import _ "unsafe"
+import (
+	"time"
+)
 
-// we will mark this to be removed soon as its not beneficial.
-// it is documented more clearly here 
-// https://golang.bg/src/runtime/time_nofake.go#:~:text=15%20var%20faketime%20int64%2016,for%20a%20fast%20monotonic%20time. 
 // nowNano returns monotonic nanoseconds since program start.
-//
-//go:linkname nowNano runtime.nanotime
-func nowNano() int64
+// Uses time.Now() which automatically handles monotonic clock.
+func nowNano() int64 {
+	return time.Now().UnixNano()
+}
